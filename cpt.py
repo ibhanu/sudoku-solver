a81dcb376a99eafbac68eb5ff1e92981ba236b15
Bhanu Pratap <pratap.bhanu3434@gmail.com>
2020-01-26 03:13:26 +0530

    added logo

---
+++ cpt.py
@@ -0,0 +1,37 @@
+import cv2 
+def capture():
+	key = cv2. waitKey(1)
+	webcam = cv2.VideoCapture(0)
+	while True:
+		try:
+			check, frame = webcam.read()
+			print(check) #prints true as long as the webcam is running
+			print(frame) #prints matrix values of each framecd 
+			cv2.imshow("Capturing", frame)
+			key = cv2.waitKey(1)
+			if key == ord('s'):
+				cv2.imwrite(filename='images_test/saved_img.jpg', img=frame)
+				webcam.release()
+				img_new = cv2.imread('images_test/saved_img.jpg', cv2.IMREAD_GRAYSCALE)
+				img_new = cv2.imshow("Captured Image", img_new)
+				cv2.waitKey(1650)
+				cv2.destroyAllWindows()        
+				capture()
+			elif key == ord('q'):
+				print("Turning off camera.")
+				webcam.release()
+				print("Camera off.")
+				print("Program ended.")
+				cv2.destroyAllWindows()
+				break
+			
+		except(KeyboardInterrupt):
+			print("Turning off camera.")
+			webcam.release()
+			print("Camera off.")
+			print("Program ended.")
+			cv2.destroyAllWindows()
+			break
+    
+if __name__ == '__main__':
+	capture()

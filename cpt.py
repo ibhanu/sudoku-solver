527b39cd09270be9575331e5b5a4a9f8881d4961
abhayrm20 <abhayrm20@gmail.com>
2020-01-26 11:07:41 +0530

    Added webcam function

--- cpt.py
+++ cpt.py
@@ -1,4 +1,6 @@
 import cv2 
+from main_img import main_process_img
+from tensorflow.keras.models import load_model
 def capture():
 	key = cv2. waitKey(1)
 	webcam = cv2.VideoCapture(0)
@@ -14,9 +16,12 @@ def capture():
 				webcam.release()
 				img_new = cv2.imread('images_test/saved_img.jpg', cv2.IMREAD_GRAYSCALE)
 				img_new = cv2.imshow("Captured Image", img_new)
-				cv2.waitKey(1650)
-				cv2.destroyAllWindows()        
-				capture()
+				im_path = 'images_test/saved_img.jpg'
+				model = load_model('model/my_model.h5')
+				main_process_img(im_path, model, save=True, display=True)
+				#cv2.waitKey(1650)
+				#cv2.destroyAllWindows()        
+				#capture()
 			elif key == ord('q'):
 				print("Turning off camera.")
 				webcam.release()
---
+++ webrelease.py
@@ -0,0 +1,3 @@
+import cv2
+webcam = cv2.VideoCapture(0)
+webcam.release()
